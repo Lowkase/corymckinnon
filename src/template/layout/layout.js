5f070c0a784b81549fd068f5ceeed4dc2ac17295
Cory <lowkase@gmail.com>
2022-11-21 12:50:49 -0500

    cleaning

--- src/template/layout/layout.js
+++ src/template/layout/layout.js
@@ -13,6 +13,7 @@ const Layout = (props) => {
                 <meta name="keywords" content="Cory Mckinnon, design, designer, product design, portfolio, ui/ux, ux" />
                 <meta name="description" content="Cory Mckinnon, product designer, lead ui/ux. Complex design problems simplified. I'm a Designer with a passion for complex systems, big data, AI/ML and solving problems." />
                 <meta http-equiv="content-language" content="en-us" />
+                <meta name="google-site-verification" content="tuWwpNBlCQtbM7LusfNRtfgvTps476Ib-ue1Uml4DDU" />
                 <link rel="icon" href={Favicon} sizes="any" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:thin,light,regular,bold,black" />
             </Helmet>
